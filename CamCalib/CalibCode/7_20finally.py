b17dbc2284c76e4b276336441580518852c4ccef
SunL1GHT <garygu9426@gmail.com>
2020-07-21 15:01:30 +0800

    update true

--- CamCalib/CalibCode/7_20finally.py
+++ CamCalib/CalibCode/7_20finally.py
@@ -1,12 +1,12 @@
 """
-    参考自CalibPhoto文档，删除了一些不必要的注释
+    参考自CalibPhoto文档，加入坐标系绘制
 """
 import cv2
 import numpy as np
 import glob
 
 def calibrate():
-    criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001)
+    criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.0001)
     objp = np.zeros((Nx_cor * Ny_cor, 3), np.float32)
     objp[:, :2] = np.mgrid[0:Nx_cor*5:5, 0:Ny_cor*5:5].T.reshape(-1, 2)
     obj_points = []
@@ -32,8 +32,21 @@ def calibrate():
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
     np.savez('../Calibresult/calibrate.npz', mtx=mtx, dist=dist[0:4])
 
+def draw_axis(img, corners, imgpts):
+        corner = tuple(corners[0].ravel())
+        img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+        cv2.putText(img, "x", tuple(imgpts[0].ravel()), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+        cv2.putText(img, "y", tuple(imgpts[1].ravel()), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+        img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+        cv2.putText(img, "z", tuple(imgpts[2].ravel()), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        return img
+
 def Transfer():
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # 绘制坐标系
+    axis_axis = np.float32([[20,0,0], [0,20,0], [0,0,-20]]).reshape(-1,3)
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.0001)
     objp = np.zeros((Nx_cor * Ny_cor, 3), np.float32)
     objp[:, :2] = np.mgrid[0:Nx_cor * 5:5, 0:Ny_cor * 5:5].T.reshape(-1, 2)
     images = glob.glob('../Calibsource/Transfer.jpg')
@@ -48,8 +61,20 @@ def Transfer():
 
     global rmtx,tmtx
     if ret:
-        imgp = cv2.cornerSubPix(gray, corners, (1, 1), (-1, -1), criteria) # 亚像素点
+        imgp_temp = cv2.cornerSubPix(gray, corners, (5, 5), (1, 1), criteria) # 亚像素点
+        if imgp_temp[0][0][0] < imgp_temp[1][0][0]:# 判断是否按照由左到右的顺序
+            imgp = imgp_temp
+        else:
+            imgp = np.flipud(imgp_temp)
+
         ret, rmtx, tmtx, inliers = cv2.solvePnPRansac(objp, imgp, mtx, dist)
+
+        # project 3D points to image plane
+        imgpts, jac = cv2.projectPoints(axis_axis, rmtx, tmtx, mtx, dist)
+        img = draw_axis(img, imgp, imgpts)
+        cv2.imshow('img',img)
+        cv2.waitKey(2000)
+        cv2.imwrite('../Calibresult/gesture.png', img)
 
     print('imgp:',imgp[0])
     print('world:',cameraToWorld(mtx,rmtx,tmtx,imgp[0][0]))
@@ -67,25 +92,41 @@ def DetectCircle(matrix, Rmatrix, tmatrix):
     imgGray = cv2.cvtColor(imgroi, cv2.COLOR_BGR2GRAY)
     ret, binary = cv2.threshold(imgGray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    Cx = 0
+    Cy = 0
+    angel = 0
 
     find = False # 是否检测到圆的标志,False--->未检测到圆，True--->检测到圆
-    # Rx = 0
-    # Ry = 0
-    # Cx = 0
-    # Cy = 0
-    # angle = 0
-
     for cnt in contours:
         area = cv2.contourArea(cnt)
+        # 工件大圆检测
         if 1000 < area < 500000:
             cv2.drawContours(imgroi, cnt, -1, (255, 30, 255), 2)  # 绘制外轮廓
             (Cx, Cy), radius = cv2.minEnclosingCircle(cnt)
             Xcenter = (int(Cx), int(Cy))
-            Sx = Cx + 320
-            Sy = Cy
-            circle_point = np.array([Sx, Sy], dtype=np.float32)
+            circle_point = np.array([Cx + 320, Cy], dtype=np.float32)
             cv2.circle(imgroi, Xcenter, int(radius), (255, 0, 0), 2)
             find = True
+
+        # 工件小圆检测
+        if 100 < area < 1000:
+            cv2.drawContours(imgroi, cnt, -1, (255, 0, 255), 2)  # 绘制外轮廓
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            x, y, w, h = cv2.boundingRect(approx)
+            cv2.rectangle(imgroi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            Rx = x + w / 2
+            Ry = y + h / 2
+            Ycenter = (int(Rx), int(Ry))
+            # print("圆心坐标为:", Ycenter)
+            cv2.line(imgroi, (int(Cx), int(Cy)), (int(Rx), int(Ry)), (0, 255, 0), 2)
+            a = np.array([int(Rx - Cx), int(Ry - Cy)])
+            b = np.array([int(Cx), 0])
+            cosangle = a.dot(b) / (np.linalg.norm(a) * np.linalg.norm(b))
+            angle = np.arccos(cosangle) * 57.3
+            if Cy < Ry:
+                angle = 360 - angle
+            cv2.putText(imgroi, '{}'.format(angle), Ycenter, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
 
     if find:
         print('圆心检测成功!')
@@ -151,15 +192,15 @@ if __name__ == '__main__':
         print("重新标定相机...")
         calibrate()
 
-    # 获取外参
-    try:
-        npzfile1 = np.load('../Calibresult/Transfer.npz')
-        rmtx = npzfile1['rmtx']
-        tmtx = npzfile1['tmtx']
-    except IOError:
-        print("重新标定外参...")
-        Transfer()
-
+    # # 获取外参
+    # try:
+    #     npzfile1 = np.load('../Calibresult/Transfer.npz')
+    #     rmtx = npzfile1['rmtx']
+    #     tmtx = npzfile1['tmtx']
+    # except IOError:
+    #     print("重新标定外参...")
+    #     Transfer()
+    Transfer()
     DetectCircle(mtx, rmtx, tmtx)
 
     cv2.destroyAllWindows()
