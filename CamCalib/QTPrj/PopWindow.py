63fde8f291a0a35d8a25f3232960224b97533c32
SunL1GHT <garygu9426@gmail.com>
2020-07-22 13:50:58 +0800

    update finally

--- CamCalib/QTPrj/PopWindow.py
+++ CamCalib/QTPrj/PopWindow.py
@@ -1,43 +1,59 @@
-from PySide2 import QtWidgets
-import sys
+from PySide2.QtWidgets import QWidget, QPushButton, QApplication, QMessageBox, QGridLayout
+from PySide2.QtGui import QIcon
+from PySide2.QtCore import Signal, Slot
 
-class MyDialog(QtWidgets.QDialog):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('模式对话框')
+class MyWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-        self.resize(500, 400)
-        self.textEdit = QtWidgets.QPlainTextEdit(self)
-        self.textEdit.setPlaceholderText("请输入薪资表")
-        self.textEdit.move(10, 25)
-        self.textEdit.resize(300, 350)
+        self.btn_dialog01 = QPushButton('弹出自定义关于消息框')
+        self.btn_dialog01.clicked.connect(self.openMessageBox01)
+        self.btn_dialog02 = QPushButton('弹出关于Qt信息')
+        self.btn_dialog02.clicked.connect(self.openMessageBox02)
+        self.btn_dialog03 = QPushButton('弹出危急消息框')
+        self.btn_dialog03.clicked.connect(self.openMessageBox03)
+        self.btn_dialog04 = QPushButton('弹出信息消息框')
+        self.btn_dialog04.clicked.connect(self.openMessageBox04)
+        self.btn_dialog05 = QPushButton('弹出询问消息框')
+        self.btn_dialog05.clicked.connect(self.openMessageBox05)
+        self.btn_dialog06 = QPushButton('弹出警告消息框')
+        self.btn_dialog06.clicked.connect(self.openMessageBox06)
 
-        self.button = QtWidgets.QPushButton('统计', self)
-        self.button.move(380, 80)
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.btn_dialog01, 1, 1)
+        self.layout.addWidget(self.btn_dialog02, 1, 2)
+        self.layout.addWidget(self.btn_dialog03, 1, 3)
+        self.layout.addWidget(self.btn_dialog04, 2, 1)
+        self.layout.addWidget(self.btn_dialog05, 2, 2)
+        self.layout.addWidget(self.btn_dialog06, 2, 3)
+        self.setLayout(self.layout)
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('主窗口')
+    @Slot()
+    def openMessageBox01(self):
+        QMessageBox.about(self, '我是标题', '我是内容')
 
-        centralWidget = QtWidgets.QWidget()
-        self.setCentralWidget(centralWidget)
+    @Slot()
+    def openMessageBox02(self):
+        QMessageBox.aboutQt(self)
 
-        button = QtWidgets.QPushButton('打开模式对话框')
-        button.clicked.connect(self.open_new_window)
+    @Slot()
+    def openMessageBox03(self):
+        QMessageBox.critical(self, '我是标题', '我是内容')
 
-        grid = QtWidgets.QGridLayout(centralWidget)
-        grid.addWidget(button)
+    @Slot()
+    def openMessageBox04(self):
+        QMessageBox.information(self, '我是标题', '我是内容')
 
-    def open_new_window(self):
-        # 实例化一个对话框类
-        self.dlg = MyDialog()        
-        # 显示对话框，代码阻塞在这里，
-        # 等待对话框关闭后，才能继续往后执行
-        self.dlg.exec_()
+    @Slot()
+    def openMessageBox05(self):
+        print(QMessageBox.question(self, '我是标题', '我是内容'))
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+    @Slot()
+    def openMessageBox06(self):
+        QMessageBox().warning(self, '我是标题', '我是内容')
+
+
+app = QApplication()
+widget = MyWidget()
+widget.show()
+app.exec_()
